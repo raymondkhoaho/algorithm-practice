00b95fbcd3dc708a45f30928f30b96fe88cd00cd
Raymond Ho <raymondkhoaho@gmail.com>
2022-11-24 07:30:21 +0000

    Completed intro-1-add algorithm.

---
+++ code-signal/add.js
@@ -0,0 +1,12 @@
+// Write a function that returns the sum of two numbers.
+
+function solution(param1, param2) {
+  var sum = param1 + param2;
+  return sum;
+}
+
+// tests
+
+solution(1, 2); // expected output: 3
+solution(1, 1000); // expected output: 1000
+solution(2, -39); // expected output: -37
